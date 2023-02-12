c0ec58ed4716003a5f401244fe8b5969ecd0028f
Nahuel <nahuelarancibia4@gmail.com>
2023-02-12 16:28:10 -0300

    backup3

--- proyecto-bybanck/src/cuenta.java
+++ proyecto-bybanck/src/cuenta.java
@@ -1,15 +1,24 @@
 //entiendad cuenta
 class cuenta {
   double saldo;
+  private double saldoPrivado;
   int agencia;
+  private int agenciaPrivate;
   int numero;
-  objetoCliente titular;
+  private double numeroPrivate;
+  objetoCliente titular = new objetoCliente();
 
+  public double getObtenerSaldoPublic() {
+    return this.saldo;
+  }
+
+  // publico DEPOSITAR
   public void depositar(double valor) {
     this.saldo += valor;
     // saldo = saldo + valor;
   }
 
+  // publico RETIRAR
   public boolean retirar(double valor) {
     if (this.saldo >= valor) {
       this.saldo -= valor;
@@ -21,10 +30,60 @@ class cuenta {
     }
   }
 
+  // publico TRANSFERIR
   public boolean transferir(double valor, cuenta destino) {
     if (this.saldo >= valor) {
       this.saldo -= valor;
       // destino.saldo += valor;
+      destino.depositar(valor);
+      System.out.println("Transferencia exitosa");
+      return true;
+    } else {
+      System.out.println("No tiene saldo suficiente");
+      return false;
+    }
+  }
+
+  // ------------------------------------------------
+  // PRIVADO
+
+  public double getObtenerSaldoPrivate() {
+    return this.saldoPrivado;
+  }
+
+  public void setAgenciaPrivate(int agenciaPrivate) {
+    if (agenciaPrivate > 0) {
+      this.agenciaPrivate = agenciaPrivate;
+    }
+  }
+
+  public int getAgencia() {
+    return agenciaPrivate;
+  }
+
+  // depositar
+  public void depositarPrivado(double valor) {
+    this.saldoPrivado += valor;
+    // saldo = saldo + valor;
+  }
+
+  // retirar
+  public boolean retirarPrivado(double valor) {
+    if (this.saldoPrivado >= valor) {
+      this.saldoPrivado -= valor;
+      System.out.println("Retiro exitoso");
+      return true;
+    } else {
+      System.out.println("No tiene saldo suficiente");
+      return false;
+    }
+  }
+
+  // private TRANSFERIR
+  public boolean transferirPrivado(double valor, cuenta destino) {
+    if (this.saldoPrivado >= valor) {
+      this.saldoPrivado -= valor;
+      // destino.saldoPrivado += valor;
       destino.depositar(valor);
       System.out.println("Transferencia exitosa");
       return true;
--- proyecto-bybanck/src/testReferencia3.java
+++ proyecto-bybanck/src/testReferencia3.java
@@ -1,10 +1,16 @@
 public class testReferencia3 {
   //Estructura correcta
+  //en este caso el objeto. contenido se inicializa en su propia clase
+  //cada vez que se cree un objeto de tipo cuenta, se va crear un objeto de tipo objetoCliente
   public static void main(String[] args) { // Abreviatura main y TAB
     cuenta cuentaDiego = new cuenta();
-    objetoCliente cliente = new objetoCliente();
-    cuentaDiego.titular = cliente;
+    // objetoCliente cliente = new objetoCliente();
+    // cuentaDiego.titular = cliente;
+    // cuentaDiego.titular = new objetoCliente();
 
+    cuentaDiego.titular.nombre = "Diego";
     System.out.println(cuentaDiego.titular.nombre); // Abreviatura sout y tab
+    System.out.println(cuentaDiego.titular);
+    System.out.println(cuentaDiego);
   }
 }
